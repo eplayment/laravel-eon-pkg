1c466dc53bc12dc48a3165716b4a7b015645e749
Airhon Carlo Cortez <acgcortez@multisyscorp.com>
2021-03-16 11:33:30 +0800

    add dropdown list and searches

--- src/Wallet.php
+++ src/Wallet.php
@@ -25,4 +25,39 @@ class Wallet
 
         return $response->json();
     }
+
+    public function getEmploymentStatus($request)
+    {
+        $response = $this->client->get($this->endpoint . '/employment-status', $request->all());
+
+        return $response->json();
+    }
+
+    public function getEmploymentNatureOfWork($request)
+    {
+        $response = $this->client->get($this->endpoint . '/employment-nature-of-work', $request->all());
+
+        return $response->json();
+    }
+
+    public function getGender($request)
+    {
+        $response = $this->client->get($this->endpoint . '/gender', $request->all());
+
+        return $response->json();
+    }
+
+    public function getSourceOfFund($request)
+    {
+        $response = $this->client->get($this->endpoint . '/source-of-fund', $request->all());
+
+        return $response->json();
+    }
+
+    public function getCivilStatus($request)
+    {
+        $response = $this->client->get($this->endpoint . '/civil-status', $request->all());
+
+        return $response->json();
+    }
 }
